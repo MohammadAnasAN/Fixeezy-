a37b74d1ff08c22cf729afa636e191d1884cb68b
MohammadAnasAN <mohammadanasan@gmail.com>
2025-04-11 19:53:12 +0530

    ui changes on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,8 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [token, setToken] = useState(true);
 
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
     <div
       className="flex items-center  justify-between py-4 fixed z-50  mb-5 top-0 left-0 right-0 bg-gradient-to-b from-zinc-900  via-gray-300 to-zinc-400 animate-border"
@@ -56,7 +58,7 @@ const Navbar = () => {
 
       <div className="mx-10 flex items-center gap-4">
         {token ? (
-          <div className="flex items-center gap-2 cursor-pointer group relative">
+          <div className="flex  items-center gap-2 cursor-pointer group relative">
             <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
@@ -90,6 +92,25 @@ const Navbar = () => {
             Create Account
           </button>
         )}
+         <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+                {/* ------------------ Mobile Menu --------------------------- */}
+                <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+                    <div className='flex items-center justify-between px-5 py-6 bg-gradient-to-b from-zinc-600  via-gray-300 to-zinc-400'>
+                      <div className="flex items-center mx-10 ">
+                          <img onClick={() => navigate('/')} src={assets.logooo} alt="logo" className="w-12 h-12" />
+                          <span onClick={() => navigate('/')} className="ml-3 text-3xl  font-bold text-white tracking-wide text-shadow-md transform hover:scale-105 transition-all ease-in-out duration-300">
+                          Fixeezy
+                          </span>
+                        </div>
+                        <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+                    </div>
+                    <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+                        <NavLink onClick={() => setShowMenu(false)} to='/' ><p className={'px-4 py-2 rounded inline-block'}>HOME</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)} to='/workers' ><p className={'px-4 py-2 rounded inline-block'}>ALL WORKERS</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)} to='/about' ><p className={'px-4 py-2 rounded inline-block'}>ABOUT</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)} to='/contact' ><p className={'px-4 py-2 rounded inline-block'}>CONTACT</p></NavLink>
+                    </ul>
+                </div>
       </div>
     </div>
   );
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
   return (
